9c4890de9e7f8f7ae192f0b2f45cb938e39773b8
Tufan Barış Yıldırım <tufanbarisyildirim@gmail.com>
2023-01-22 13:59:07 +0300

    init balancing strategies

--- src/upstream.rs
+++ src/upstream.rs
@@ -16,3 +16,42 @@ impl Upstream {
         }
     }
 }
+//
+// struct Upstreams {
+//     upstreams: Vec<Upstream>,
+//     strategy: Box<dyn BalancerStrategy>,
+// }
+//
+// impl Upstreams {
+//     fn new(destinations: Vec<&str>) -> Self {
+//         let upstreams = destinations.iter().map(|d| Upstream::new(d)).collect();
+//         let strategy = Box::new(RoundRobinBalancer::new());
+//         Upstreams { upstreams, strategy }
+//     }
+//
+//     fn get_next_upstream(&mut self) -> Option<&mut Upstream> {
+//         self.strategy.get_next_upstream(&mut self.upstreams)
+//     }
+// }
+//
+// trait BalancerStrategy {
+//     fn get_next_upstream(&mut self, upstreams: &mut Vec<Upstream>) -> Option<&mut Upstream>;
+// }
+//
+// struct RoundRobinBalancer {
+//     current_index: usize,
+// }
+//
+// impl RoundRobinBalancer {
+//     fn new() -> Self {
+//         RoundRobinBalancer { current_index: 0 }
+//     }
+// }
+//
+// impl BalancerStrategy for RoundRobinBalancer {
+//     fn get_next_upstream(&mut self, upstreams: &mut Vec<Upstream>) -> Option<&mut Upstream> {
+//         let upstream = upstreams.get_mut(self.current_index % upstreams.len())?;
+//         self.current_index += 1;
+//         Some(upstream)
+//     }
+// }
